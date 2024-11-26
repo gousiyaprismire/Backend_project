dc3cae1f944e90b1b9cd6fb45cc5bf996ffb5e62
RamyaBonu <ramyabonu104@gmail.com>
2024-11-26 17:42:35 +0530

    Update AdminController.java

--- University-Web-App-Development/src/main/java/com/university/controller/AdminController.java
+++ University-Web-App-Development/src/main/java/com/university/controller/AdminController.java
@@ -20,26 +20,6 @@ public class AdminController {
         return new ResponseEntity<>(createdAdmin, HttpStatus.CREATED);
     }
 
-//    @GetMapping("/findByEmail")
-//    public ResponseEntity<?> findByEmail(@RequestParam String email) {
-//        Admin admin = adminServiceImpl.findAdminByEmail(email);
-//        if (admin != null) {
-//            return new ResponseEntity<>(admin, HttpStatus.OK);
-//        } else {
-//            return new ResponseEntity<>("Record not found for email: " + email, HttpStatus.NOT_FOUND);
-//        }
-//    }
-//
-//    @PostMapping("/login")                                        // http://localhost:8080/api/admin/login?email=Admin-2@example.com&password=password2
-//    public ResponseEntity<String> login(@RequestParam String email, @RequestParam String password) {
-//        boolean isValid = adminServiceImpl.validateLogin(email, password);
-//        if (isValid) {
-//            return new ResponseEntity<>("Successfully logged in!", HttpStatus.OK);
-//        } else {
-//            return new ResponseEntity<>("Invalid email or password!", HttpStatus.UNAUTHORIZED);
-//        }
-//    }
-
 
     @GetMapping("/findByEmail/{email}")
     public ResponseEntity<?> findByEmail(@PathVariable String email) {
