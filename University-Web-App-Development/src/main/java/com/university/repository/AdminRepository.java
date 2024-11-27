d60bf3954562a768ff12fb8fb8a08cfbd8445425
gousiyaprismire <gousiyaprismire@gmail.com>
2024-11-27 09:52:16 +0530

    Update AdminRepository.java

--- University-Web-App-Development/src/main/java/com/university/repository/AdminRepository.java
+++ University-Web-App-Development/src/main/java/com/university/repository/AdminRepository.java
@@ -1,13 +1,12 @@
 package com.university.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.university.model.Admin;
 
 @Repository
-public interface AdminRepository extends CrudRepository<Admin,Integer> {
-    public Admin findByName(String name);
+public interface AdminRepository extends JpaRepository<Admin,Integer>
+{
+    public Admin findByEmail(String email);
 }
-
-
