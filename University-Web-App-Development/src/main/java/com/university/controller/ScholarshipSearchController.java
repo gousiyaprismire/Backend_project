42b6441f4c4c1b659df7e15a6ca3bf71ce2d64f8
balunali <balunali0421@gmail.com>
2024-11-28 10:07:38 +0530

    Implemeted scholorship controller

---
+++ University-Web-App-Development/src/main/java/com/university/controller/ScholarshipSearchController.java
@@ -0,0 +1,28 @@
+package com.university.controller;
+
+import com.university.DTO.ScholarshipSearchDTO;
+import com.university.serviceImpl.ScholarshipServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/college/scholarships/search")
+public class ScholarshipSearchController {
+
+    @Autowired
+    private ScholarshipServiceImpl scholarshipService;
+
+    @PostMapping
+    public List<ScholarshipSearchDTO> searchScholarships(@RequestBody ScholarshipSearchDTO searchDTO) {
+        return scholarshipService.searchScholarships(searchDTO.getCourseName(), searchDTO.getLevel());
+    }
+
+    @GetMapping
+    public List<ScholarshipSearchDTO> searchScholarships(
+            @RequestParam(value = "type", required = false) String type,
+            @RequestParam(value = "eligibility", required = false) String eligibility) {
+        return scholarshipService.searchScholarships(type, eligibility);
+    }
+}
--- University-Web-App-Development/src/main/java/com/university/repository/ScholarshipRepository.java
+++ University-Web-App-Development/src/main/java/com/university/repository/ScholarshipRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ScholarshipRepository extends JpaRepository<Scholarship,Long> {
-    List<Scholarship> findByTypeAndEligibility(String type, String eligibility);
 }
