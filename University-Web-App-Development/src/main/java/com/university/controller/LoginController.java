cf11a7048ada3530fcaeb35886ee2285d5d5882e
DESKTOP-AER0LBM\$mi!e <balunali0421@gmail.com>
2024-12-12 18:46:30 +0530

    connectivity with frontend

--- University-Web-App-Development/src/main/java/com/university/controller/LoginController.java
+++ University-Web-App-Development/src/main/java/com/university/controller/LoginController.java
@@ -3,14 +3,12 @@ package com.university.controller;
 
 import lombok.AllArgsConstructor;
 import com.university.DTO.LoginResponse;
-import com.university.model.Student;
-import com.university.repository.StudentRepository;
+import com.university.model.StudentRegistration;
+import com.university.repository.StudentRegistrationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/students")
@@ -18,17 +16,17 @@ import java.util.UUID;
 public class LoginController {
 
     @Autowired
-    private StudentRepository studentRepository;
+    private StudentRegistrationRepository studentRepository;
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody Student student) {
+    public ResponseEntity<LoginResponse> login(@RequestBody StudentRegistration student) {
         System.out.println("Login attempt for student: " + student.getEmail());
 
-        Student existingStudent = studentRepository.findByEmail(student.getEmail());
+        StudentRegistration existingStudent = studentRepository.findByEmail(student.getEmail());
 
         if (existingStudent != null) {
             return ResponseEntity.status(HttpStatus.OK)
-                    .body(new LoginResponse("success", existingStudent.getStudentId(), "success"));
+                    .body(new LoginResponse("success", existingStudent.getId(), "success"));
         }
 
 
@@ -36,16 +34,16 @@ public class LoginController {
         studentRepository.save(student);
 
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(new LoginResponse("success", student.getStudentId(), "success"));
+                .body(new LoginResponse("success", student.getId(), "success"));
     }
 
     @GetMapping("/login")
     public ResponseEntity<LoginResponse> getStudentById(@RequestParam Long studentId) {
-        Student existingStudent = studentRepository.findById(studentId).orElse(null);
+        StudentRegistration existingStudent = studentRepository.findById(studentId).orElse(null);
 
         if (existingStudent != null) {
             return ResponseEntity.status(HttpStatus.OK)
-                    .body(new LoginResponse("success", existingStudent.getStudentId(), "success"));
+                    .body(new LoginResponse("success", existingStudent.getId(), "success"));
         }
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
